51209d5f7777360dec94e32ea1687950a6013d59
Michelle Raouf <michelle.raouf@outlook.com>
2024-08-09 21:13:02 +0300

    remove imports not need

--- sample/composeApp/src/commonMain/kotlin/io/github/sample/App.kt
+++ sample/composeApp/src/commonMain/kotlin/io/github/sample/App.kt
@@ -3,9 +3,6 @@ package io.github.sample
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.KeyboardArrowDown
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.lightColorScheme
